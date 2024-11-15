9af44d1c87b667bb9ef8d503fa12f9afcb491ec3
IsabellaTito <isabellatito76@gmail.com>
2024-11-15 19:58:25 -0300

    Acrescentando outra forma de resolver

--- Lista_1/Q30.cpp
+++ Lista_1/Q30.cpp
@@ -21,3 +21,17 @@ int main(){
     std::cout << min(s) << std::endl;
     return 0;
 }
+
+/*
+
+void sm(string &palavra){
+    int i{0};
+    while(palavra[i] != '\0'){
+        if(palavra[i]>='A' && palavra[i]<='Z'){
+            palavra[i]+=32; \\As letras minusculas vem primeiro na tabela ASCII e depois as maiusculas. Então basicamente ele pegou o valor da letra e somou 32 para chegar na sua respectiva maiuscula    
+        }
+        i++;
+    }
+}
+
+*/
